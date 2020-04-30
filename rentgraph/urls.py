3c0fd2b60d6bba1117a571e6c058f05983f8e590
Bendoo1 <bendoo12345@gmail.com>
2020-04-30 16:54:03 +0000

    Final deploy

--- rentgraph/urls.py
+++ rentgraph/urls.py
@@ -21,12 +21,13 @@ from posts import urls as urls_posts
 from confirm import urls as urls_confirm
 from cart import urls as urls_cart
 from feedback import urls as urls_feedback
+from posts.views import all_posts
 from django.views import static
 from .settings import MEDIA_ROOT
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', RedirectView.as_view(url='posts/')),
+    url(r'^$', all_posts, name='index'),
     url(r'posts/', include(urls_posts)),
     url(r'^accounts/', include(urls_accounts)),
     url(r'^confirm/', include(urls_confirm)),
